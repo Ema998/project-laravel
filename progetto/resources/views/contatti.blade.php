1636891228bfc295aafe2e0bfb1708dd48afbd0b
Ema998 <emanuele.gornati3@gmail.com>
2025-05-04 21:48:00 +0200

    Initial commit

---
+++ progetto/resources/views/contatti.blade.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>contatti</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+        <link rel="stylesheet" href="/style.css">
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg bg-dark border-bottom" data-bs-theme="dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"><i class="bi bi-controller"></i></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/">Home</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="/chi-siamo">Chi Siamo</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="/contatti">Contatti</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <header class="container-fluid">
+            <div class="row h-100 align-items-center justify-content-around">
+                <div class="col-12 h-25 justify-content-center align-items-center">
+                    <h3 class="text-color text-center display-5">CONTATTI</h3>
+                </div>
+                <div class="col-md-3 text-center box d-flex flex-column justify-content-center align-items-center">
+                    <div class="row">
+                    <div class="col-12">
+                        <i class="icon bi bi-whatsapp"></i>
+                    </div>
+                    </div>
+                    <div class="row">
+                    <div class="col-12">
+                        <p>contattaci su whatsapp</p>
+                    </div>
+                    </div>
+                </div>
+                <div class="col-md-3 text-center box d-flex flex-column justify-content-center align-items-center">
+                <div class="row">
+                <div class="col-12">
+                        <i class="icon bi bi-instagram"></i>
+                    </div>
+                    </div>
+                    <div class="row">
+                    <div class="col-12">
+                        <p>seguici su instagram</p>
+                    </div>
+                    </div>
+                </div>
+                <div class="col-md-3 text-center box d-flex flex-column justify-content-center align-items-center">
+                    <div class="row">
+                        <div class="col-12">
+                            <i class="icon bi bi-facebook"></i>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <p>seguici su facebook</p>
+                        </div>
+                    </div>
+                </div>
+        </header>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ progetto/resources/views/index.blade.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+  <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>gameland</title>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+      <link rel="stylesheet" href="/style.css">
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg bg-dark border-bottom" data-bs-theme="dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#"><i class="bi bi-controller"></i></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="/">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/chi-siamo">Chi Siamo</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/contatti">Contatti</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Dropdown
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+            </li> 
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <header class="container-fluid">
+        <div class="row h-100">
+            <div class="col-12 d-flex justify-content-center align-items-center">
+                <h1 class="fw-bold text-center display-1 text-color">GAMELAND</h1>
+            </div>
+        </div>
+    </header>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
+  </body>
+</html>
